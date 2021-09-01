8f8d0c449dc57b5cae36a20d3c1a90ed6dcd326f
Shariq Ansari <shariqansari1819@gmail.com>
2021-09-01 16:05:46 +0500

    Add register user, community and create text post methods.

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<amity_sdk_flutter/AmitySdkFlutterPlugin.h>)
+#import <amity_sdk_flutter/AmitySdkFlutterPlugin.h>
+#else
+@import amity_sdk_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AmitySdkFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"AmitySdkFlutterPlugin"]];
+}
+
+@end
--- ios/Classes/SwiftAmitySdkFlutterPlugin.swift
+++ ios/Classes/SwiftAmitySdkFlutterPlugin.swift
@@ -1,7 +1,13 @@
 import Flutter
 import UIKit
+import AmitySDK
+
 
 public class SwiftAmitySdkFlutterPlugin: NSObject, FlutterPlugin {
+
+    var client: AmityClient?
+
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "amity_sdk_flutter", binaryMessenger: registrar.messenger())
     let instance = SwiftAmitySdkFlutterPlugin()
@@ -9,6 +15,64 @@ public class SwiftAmitySdkFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if(call.method == "getPlatformVersion"){
+        result("iOS " + UIDevice.current.systemVersion)
+    }else if(call.method == "registerApp"){
+        let params = call.arguments as! NSDictionary
+        client = AmityClient(apiKey: params["apiKey"] as! String)
+    }else if(call.method == "authenticateUser"){
+        let params = call.arguments as! NSDictionary
+        if(client != nil){
+            client?.registerDevice(withUserId: params["userId"] as! String, displayName: params["userName"] as? String, authToken: nil, completion: { (success, error) in
+                if(success){
+                    result(true)
+                }else{
+                    result(false)
+                }
+            })
+        }else{
+            result(FlutterError(code: "SDKNotInititialized", message: "Amity SDK not initialized", details: "Please initialize SDK by calling registerApp method first."))
+        }
+    }else if(call.method == "signOut"){
+        if(client != nil){
+            client?.unregisterDevice()
+            result(true)
+        }else{
+            result(FlutterError(code: "SDKNotInititialized", message: "Amity SDK not initialized", details: "Please initialize SDK by calling registerApp method first."))
+        }
+    }else if(call.method == "createCommunity"){
+        let params = call.arguments as! NSDictionary
+        let builder = AmityCommunityCreationDataBuilder()
+        builder.setDisplayName(params["communityName"] as! String)
+        builder.setIsPublic(true)
+        if(client != nil){
+            let communityRepository = AmityCommunityRepository.init(client: client!)
+            communityRepository.createCommunity(with: builder) { (community, error) in
+                if(error != nil){
+                    result(true)
+                }else{
+                    result(false)
+                }
+            }
+        }
+    }else if(call.method == "joinCommunity"){
+        let params = call.arguments as! NSDictionary
+
+        let communityRepository = AmityCommunityRepository.init(client: client!)
+        communityRepository.joinCommunity(withId: params["communityId"] as! String) { (success, error) in
+            result(success)
+        }
+    }else if(call.method == "createTextPost"){
+        let params = call.arguments as! NSDictionary
+        let postBuilder = AmityTextPostBuilder()
+        postBuilder.setText(params["postText"] as! String)
+        let postRepository = AmityPostRepository.init(client: client!)
+        postRepository.createPost(postBuilder, targetId: nil, targetType: AmityPostTargetType.community) { (post, error) in
+            result(error != nil)
+        }
+    }else if(call.method == "createImageWithTextPost"){
+
+    }
+    
   }
 }
